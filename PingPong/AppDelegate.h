5ea1e9374b9d345d03e831f82203252f60ef4777
iukarpov <spangegoth@gmail.com>
2019-04-04 22:09:48 +0300

    Initial Commit

---
+++ PingPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by Igor on 04/04/2019.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PingPong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PingPong
+//
+//  Created by Igor on 04/04/2019.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
